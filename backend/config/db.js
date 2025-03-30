5d066dd14da1b98b1114aba6b438eda4ce922802
Manasseh Gitau <kimmanasseh14@gmail.com>
2025-03-30 22:11:11 +0300

    fix: fix api endpoints for the auth and user routes

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const  connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {});
-        console.log("Connected to MongoDB succefully")
+        console.log("Connected to MongoDB successfully")
     } catch (err) {
         console.error("Error connecting to MongoDB", err)
     }
--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,31 +1,29 @@
 const jwt = require('jsonwebtoken')
-const user = require('../models/User');
 const User = require('../models/User');
 
 //Middleware to protect routes
 const protect = async (req, res, next) => {
+    const token = req.header("Authorization")?.split(" ")[1]; // Bearer token
+    if (!token) return res.status(401).json({ message: "Access denied. No token provided" });
+    
     try {
-        let token = req.headers.authorization;
-        if (token && token.startsWith('Bearer')) {
-            token = token.split(' '); //Extract token
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = await User.findById(decoded.id).select('-password');
-            next();
-        } else {
-            res.status(401).json({ message: "Not authorized, no token" });
-        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select("-password");
+        if (!req.user) return res.status(404).json({ message: "User not found" });
+        next();
+
     } catch (error) {
-        res.status(401).json({ message: "Token failed", error: error.message})
+        res.status(401).json({ message: "Token failed", error: error.message })
     }
 }
 
 //Middleware for admin only
 const adminOnly = (req, res, next) => {
-    if ( req.user && req.user.role === 'admin' ) {
+    if (req.user && req.user.role === 'admin') {
         next()
     } else {
-        res.status(403).json({ message: 'Access denied, admin only'});
+        res.status(403).json({ message: 'Access denied, admin only' });
     }
 }
 
-module.exports = { protect, adminOnly}
+module.exports = { protect, adminOnly }
